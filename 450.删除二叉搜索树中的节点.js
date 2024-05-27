ac57f3a43d15b65ed24524046354d3752d87021b
wangzl <1498129827@qq.com>
2024-05-27 17:40:33 +0800

    fix: 450.删除二叉搜索树中的节点

--- 450.删除二叉搜索树中的节点.js
+++ 450.删除二叉搜索树中的节点.js
@@ -19,6 +19,31 @@
  * @return {TreeNode}
  */
 var deleteNode = function (root, key) {
-  
+  if (!root) return null;
+  if (key < root.val) {
+    root.left = deleteNode(root.left, key);
+  } else if (key > root.val) {
+    root.right = deleteNode(root.right, key);
+  } else {
+    // 1: 该节点是叶子节点，即左右孩子为空
+    if (!root.left && !root.right) {
+      return null;
+    }
+    // 2: 只有一个孩子节点
+    if (!root.left || !root.right) {
+      return root.left ?? root.right;
+    }
+    // 3: 左右孩子节点都存在
+    const minNode = getMinNode(root.right)
+    minNode.left = root.left
+    root = root.right;
+  }
+  return root;
 };
+function getMinNode(node) {
+  while (node.left) {
+    node = node.left;
+  }
+  return node;
+}
 // @lc code=end
